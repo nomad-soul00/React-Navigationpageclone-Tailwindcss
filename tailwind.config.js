fdad5a1cc2f720bea84c1694ccf010ad22e1e898
Sahil <sahilahamed813@gmail.com>
2025-01-09 19:38:47 +0530

    new project

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      "sm-0": "500px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    extend: {
+      colors: {
+        bars: "#242526",
+        primary:"#414360",
+        secondary:"#8874be",
+        third:"	#917fc3",
+        section: "#25292e"
+      },
+      strokeWidth: {
+        3: "3",
+      },
+    },
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};
